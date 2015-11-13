da0e6ebcacb4d042bd863ddf0cdb672ec5940736
Jas Chen <jas228@gmail.com>
2015-11-13 15:50:13 +0800

    refactor

--- webpack/style-loaders.js
+++ webpack/style-loaders.js
@@ -1,8 +1,7 @@
 'use strict';
 
+var minimize = process.env.NODE_ENV === 'production';
+
 module.exports = [
-  {
-    test: /\.css$/,
-    loader: process.env.NODE_ENV === 'production'? 'css-loader?minimize': 'css-loader'
-  }
+  { test: /\.css$/, loader: `css-loader?{minimize:${minimize}}` }
 ];
