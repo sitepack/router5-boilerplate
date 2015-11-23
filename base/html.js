fc35cf5f3f3d95ced50e2adae40bd3847cdf2284
Jas Chen <jas228@gmail.com>
2015-11-23 10:00:30 +0800

    update npm dependencies

--- base/html.js
+++ base/html.js
@@ -1,16 +1,44 @@
 'use strict';
 
-var defaultTitle = 'MySite';
+const defaultTitle = 'MySite';
+const routes = require('../config/route.js');
+const Router5 = require( 'router5' ).Router5;
+const router = new Router5(routes, {trailingSlash: true});
 
-function render(title, content) {
+function linkTag(href) {
+  return `<link rel="stylesheet" href="${href}">`;
+}
+
+function scriptTag(src) {
+  return `<script src="${src}"></script>`;
+}
+
+function render(path, content, cssHrefs, scriptSrcs) {
+  const targetRouteState = router.matchPath(path);
+  let targetRoute;
+
+  if (targetRouteState) {
+    routes.forEach((route) => {
+      if (route.name === targetRouteState.name) {
+        targetRoute = route;
+      }
+    });
+  }
+
+  if (!targetRoute) {
+    targetRoute = {};
+  }
+
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>${title || defaultTitle}</title>
+  <title>${targetRoute.title || defaultTitle}</title>
+  ${cssHrefs.map(linkTag).join('')}
 </head>
 <body>
   ${content}
+  ${scriptSrcs.map(scriptTag).join('')}
 </body>
 </html>`;
 }
